c8fbb31fdf336172182c6386f20d22d35b742c3a
Alejandro F <afuste7@gmail.com>
2020-06-04 22:06:07 -0400

    change error status in api file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,7 +7,7 @@ const db = require('../models');
 module.exports = function(app) {
 	// Each of the below routes will do a different CRUD operation.
 
-	// GET route to get user and their watchlist
+	// GET route to get all books
 	app.get('/api/books', (req, res) => {
 		db.Book
 			.find({})
@@ -15,11 +15,11 @@ module.exports = function(app) {
 				res.json(data);
 			})
 			.catch((err) => {
-				res.status(400).json(err);
+				res.status(422).json(err);
 			});
 	});
 
-	// POST route to add stock to watchlist
+	// POST route to add book to collection
 	app.post('/api/books', ({ body }, res) => {
 		db.Book
 			.create(body)
@@ -27,10 +27,11 @@ module.exports = function(app) {
 				res.json(data);
 			})
 			.catch((err) => {
-				res.status(400).json(err);
+				res.status(422).json(err);
 			});
 	});
 
+	// Delete route to remove book from collection
 	app.delete('/api/books/:id', (req, res) => {
 		db.Book
 			.deleteOne({ _id: req.params.id })
@@ -38,7 +39,7 @@ module.exports = function(app) {
 				res.json(data);
 			})
 			.catch((err) => {
-				res.status(400).json(err);
+				res.status(422).json(err);
 			});
 	});
 };
