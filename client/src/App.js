95aaae07902ad408fcee7b33702421d5724ad273
Alejandro F <afuste7@gmail.com>
2020-06-05 23:44:21 -0400

    added assets folder with image for jumbrotron background. Also, added code in index.js and style.css of jumbrotron component.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Saved from './pages/Saved';
 import Search from './pages/Search';
 import Navigation from './components/Navigation';
+import Jumbo from './components/Jumbo';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
 			<Router>
 				<div>
 					<Navigation />
+					<Jumbo />
 					<Route exact path="/" component={Search} />
 					<Route exact path="/search" component={Search} />
 					<Route exact path="/saved" component={Saved} />
