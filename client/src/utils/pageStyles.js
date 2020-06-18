29a3d6f70956613face1adcf79155f6ab670be8b
Alejandro F <afuste7@gmail.com>
2020-06-18 13:49:36 -0400

    Added pageStyles.js file to utils folder to export styling object

---
+++ client/src/utils/pageStyles.js
@@ -0,0 +1,20 @@
+export default {
+	button: {
+		margin: '10px',
+		backgroundColor: '#969797',
+		color: 'white',
+		float: 'right',
+		borderColor: '#969797'
+	},
+	li: {
+		border: '1px solid #ddd',
+		borderRadius: '4px',
+		minHeight: '45vh',
+		margin: '20px'
+	},
+	titleAndAuthor: {
+		margin: '10px 0 5px 15px'
+	},
+	img: { width: '10vw', height: '30vh', borderRadius: '3px', margin: '15px' },
+	p: { margin: '15px 0 0 10px' }
+};
