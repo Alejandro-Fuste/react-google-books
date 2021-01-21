4ece1c6df472685846397637b7e94750f9d9f6b6
Alejandro F <afuste7@gmail.com>
2021-01-21 12:27:33 -0500

    commented out console.logs from googleBooksAPI.js file

--- libs/googleBooksAPI.js
+++ libs/googleBooksAPI.js
@@ -5,11 +5,11 @@ const apiKey = process.env.API_KEY;
 module.exports = {
 	getBook: ({ query }) => {
 		// const { searchTerm } = query;
-		console.log('query from googleBooksAPI file:');
-		console.log(query);
+		// console.log('query from googleBooksAPI file:');
+		// console.log(query);
 
-		console.log('route for google book api:');
-		console.log(`${BASE_URI}${query}+intitle:${query}&key=${apiKey}`);
+		// console.log('route for google book api:');
+		// console.log(`${BASE_URI}${query}+intitle:${query}&key=${apiKey}`);
 
 		return axios.get(`${BASE_URI}${query}+intitle:${query}&key=${apiKey}`, {
 			params: {
