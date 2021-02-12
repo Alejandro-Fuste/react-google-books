d9254e18d6a32cfc0878ea90f938201a14373e0f
Alejandro F <afuste7@gmail.com>
2021-02-12 08:02:15 -0500

    added return statement to return debounceValue state value

--- client/src/utils/debounceHook.js
+++ client/src/utils/debounceHook.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 
 const useDebounce = (value, delay) => {
 	const [ debouncedValue, setDebounceValue ] = useState(value);
+
+	return debouncedValue;
 };
 
 export default useDebounce;
