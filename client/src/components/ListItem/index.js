b9eb33e708d993e4005f37c5e781c5c1cd4bdd31
Alejandro F <afuste7@gmail.com>
2021-02-18 12:35:22 -0500

    added code to return keyword to display a list item

--- client/src/components/ListItem/index.js
+++ client/src/components/ListItem/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
 
-const ListItem = (params) => {};
+const ListItem = (params) => {
+	return (
+		<React.Fragment>
+			{results ? (
+				results.map((result, index) => (
+					<div>
+						<li key={result.bookId} style={li}>
+							<Button style={button} href={result['previewLink']}>
+								View
+							</Button>
+							<Button style={button} onClick={saveBook}>
+								Save
+							</Button>
+							<h5 style={titleAndAuthor}>Title: {result['title']}</h5>
+							<h6 style={titleAndAuthor}>Author: {result['authors']}</h6>
+							<img
+								className="pic"
+								src={result.image ? result.image : NoImage}
+								alt="Book Thumbnail"
+								style={img}
+							/>
+							<p style={p}>{result['description']}</p>
+						</li>
+					</div>
+				))
+			) : (
+				<h3>No Results</h3>
+			)}
+		</React.Fragment>
+	);
+};
 
 export default ListItem;
