df54184f74ba4879ab82f50168120d60eeca5ac7
Alejandro F <afuste7@gmail.com>
2021-02-19 10:57:41 -0500

    removed ternary operator and map method from ListItem component

--- client/src/components/ListItem/index.js
+++ client/src/components/ListItem/index.js
@@ -1,38 +1,28 @@
 import React from 'react';
+import NoImage from '../assets/noImageVector.jpg';
 
 import { Button } from 'react-bootstrap';
 import Styles from '../../utils/pageStyles';
 
 const { li, button, titleAndAuthor, img, p } = Styles;
 
-const ListItem = ({ results }) => {
+const ListItem = ({ result }) => {
 	return (
 		<React.Fragment>
-			{results ? (
-				results.map((result) => (
-					<div>
-						<li key={result.bookId} style={li}>
-							<Button style={button} href={result['previewLink']}>
-								View
-							</Button>
-							<Button style={button} onClick={saveBook}>
-								Save
-							</Button>
-							<h5 style={titleAndAuthor}>Title: {result['title']}</h5>
-							<h6 style={titleAndAuthor}>Author: {result['authors']}</h6>
-							<img
-								className="pic"
-								src={result.image ? result.image : NoImage}
-								alt="Book Thumbnail"
-								style={img}
-							/>
-							<p style={p}>{result['description']}</p>
-						</li>
-					</div>
-				))
-			) : (
-				<h3>No Results</h3>
-			)}
+			<div>
+				<li key={result.bookId} style={li}>
+					<Button style={button} href={result['previewLink']}>
+						View
+					</Button>
+					<Button style={button} onClick={saveBook}>
+						Save
+					</Button>
+					<h5 style={titleAndAuthor}>Title: {result['title']}</h5>
+					<h6 style={titleAndAuthor}>Author: {result['authors']}</h6>
+					<img className="pic" src={result.image ? result.image : NoImage} alt="Book Thumbnail" style={img} />
+					<p style={p}>{result['description']}</p>
+				</li>
+			</div>
 		</React.Fragment>
 	);
 };
