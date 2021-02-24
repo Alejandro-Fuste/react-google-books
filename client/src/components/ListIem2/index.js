59fd7121a82981e40b033a9aa4d0e1a306c26d54
Alejandro F <afuste7@gmail.com>
2021-02-23 20:54:31 -0500

    added ListItem2 to components folder

---
+++ client/src/components/ListIem2/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import NoImage from '../../assets/noImageVector.jpg';
+
+import { Button } from 'react-bootstrap';
+import Styles from '../../utils/pageStyles';
+
+const { li, button, titleAndAuthor, img, p } = Styles;
+
+const ListItem = ({ result, saveBook }) => {
+	return (
+		<React.Fragment>
+			<div>
+				<li style={li}>
+					<Button style={button} href={result['previewLink']}>
+						View
+					</Button>
+					<Button style={button} onClick={saveBook}>
+						Save
+					</Button>
+					<h5 style={titleAndAuthor}>Title: {result['title']}</h5>
+					<h6 style={titleAndAuthor}>Author: {result['authors']}</h6>
+					<img className="pic" src={result.image ? result.image : NoImage} alt="Book Thumbnail" style={img} />
+					<p style={p}>{result['description']}</p>
+				</li>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default ListItem;
